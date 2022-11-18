49bc30557601da51948a5f9936366f84730af3c7
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-11-18 22:33:09 +0100

    pagos junta ok

--- src/carrillon/app.clj
+++ src/carrillon/app.clj
@@ -14,8 +14,8 @@
 
 (defn trans-of-apt [apt transactions]
   (->> transactions
-         (filter (of-io :i))
-         (filter #(= apt (:apt %)))))
+       (filter (of-io :i))
+       (filter #(= apt (:apt %)))))
 
 (defn table-str [fields cols]
   (with-out-str
@@ -23,35 +23,32 @@
 
 (defn transaction-table-str [transactions]
   (with-out-str
-     (print-table 
-       [:date :amount :from :id :sucursal]
-        transactions)))
+    (print-table
+     [:date :amount :from :id :sucursal]
+     transactions)))
 
 (defn round [nr]
-   (-> nr Math/floor int))
+  (-> nr Math/floor int))
 
 (defn calc-apartment [transactions apt]
   (let [payments  (trans-of-apt apt transactions)
         quotas (apartment-quotas apt)
         balance (concat payments quotas)
         paid (reduce + (map :amount-f payments))
-        quotas (reduce + (map :amount-f quotas))
-        ]
-   {:payments payments
-    :quotas quotas
-    :balance balance
-    :stats {:apt apt
-            :paid (round paid)
-            :quotas (round quotas)
-            :balance (round (+ paid quotas))}
-    }))
-    
+        quotas (reduce + (map :amount-f quotas))]
+    {:payments payments
+     :quotas quotas
+     :balance balance
+     :stats {:apt apt
+             :paid (round paid)
+             :quotas (round quotas)
+             :balance (round (+ paid quotas))}}))
+
 (defn print-apartment [transactions apt]
   (let [{:keys [payments balance] :as stats}  (calc-apartment transactions apt)
         table-payments (transaction-table-str payments)
-        table-balance (transaction-table-str balance)
-        ]
-    (spit (str "in-txt/" (name apt) ".txt") 
+        table-balance (transaction-table-str balance)]
+    (spit (str "in-txt/" (name apt) ".txt")
           table-payments)
     (spit (str "balance-txt/" (name apt) ".txt")
           table-balance)
@@ -63,10 +60,9 @@
                #(print-apartment transactions (first %))
                apartments)
         ;stats (map #(dissoc % :payments :quotas :balance) stats)
-        stats (map #(:stats % ) stats)
-        ]
+        stats (map #(:stats %) stats)]
     (println "stats: " stats)
-    (->> stats 
+    (->> stats
          (table-str [:apt :quotas :paid :balance])
          (spit "summary.txt"))))
 
@@ -114,6 +110,7 @@
   (let [apt (case id
               "217659184" :3C ; 2022 17 whatsapp lizca
 
+              ; banco general / montilla
               "231624416" :1D ; payment banco general debt 
               "245461181" :1D ;| 04/06/2022
               "263372009" :1D ;| 30/06/2022 |   105.00 |       |         1 |  |
@@ -143,22 +140,50 @@
               "97033971" :2C ; flor (llamada con margho, esposo pago antes del muerto)
               "89596510" :2C ; flor (llamada con margho, esposo pago antes del muerto)
               "81579441" :2C ; flor (llamada con margho, esposo pago antes del muerto)
-              
+
               ; rosemary
-                "81579210" :4A ; rosemary (whatsapp con yvette)
-                "90135366" :4A ; rosemary (whatsapp con yvette)
-                "97769041" :4A ; rosemary (whatsapp con yvette)
-                "104452340" :4A ; rosemary (whatsapp con yvette)
-                "112721490" :4A ; rosemary (whatsapp con yvette)
-                "122267762" :4A ; rosemary (whatsapp con yvette)
-                "130793685" :4A ; rosemary (whatsapp con yvette)
-                "134645655" :4A ; rosemary (whatsapp con yvette)
-                "142317086" :4A ; rosemary (whatsapp con yvette)
-                "148684351" :4A ; rosemary (whatsapp con yvette)
-                "160187688" :4A ; rosemary (whatsapp con yvette)
-                "208504118" :4A ; rosemary (whatsapp con yvette)
-                "226428240" :4A ; rosemary (whatsapp con yvette)
-                "220849650" :4A ; rosemary (whatsapp con yvette)
+              "81579210" :4A ; rosemary (whatsapp con yvette)
+              "90135366" :4A ; rosemary (whatsapp con yvette)
+              "97769041" :4A ; rosemary (whatsapp con yvette)
+              "104452340" :4A ; rosemary (whatsapp con yvette)
+              "112721490" :4A ; rosemary (whatsapp con yvette)
+              "122267762" :4A ; rosemary (whatsapp con yvette)
+              "130793685" :4A ; rosemary (whatsapp con yvette)
+              "134645655" :4A ; rosemary (whatsapp con yvette)
+              "142317086" :4A ; rosemary (whatsapp con yvette)
+              "148684351" :4A ; rosemary (whatsapp con yvette)
+              "160187688" :4A ; rosemary (whatsapp con yvette)
+              "208504118" :4A ; rosemary (whatsapp con yvette)
+              "226428240" :4A ; rosemary (whatsapp con yvette)
+              "220849650" :4A ; rosemary (whatsapp con yvette)
+              "141177110" :4A ; rosemary (whatsapp con yvette)
+              "235240157" :4A ; rosemary (whatsapp con yvette)
+              "197381349" :4A ; rosemary (whatsapp con yvette)
+              "237513903" :4A ; rosemary (whatsapp con yvette)
+              "241281237" :4A ; rosemary (whatsapp con yvette)
+              "245070233" :4A ; rosemary (whatsapp con yvette)
+              "179035353" :4A ; rosemary (whatsapp con yvette)
+
+              ; grimaldo
+              "82119575" :1A ; grimaldo (whatsapp con grimaldo)
+              "90391729" :1A ; grimaldo (whatsapp con grimaldo)
+              "99890015" :1A ; grimaldo (whatsapp con grimaldo)
+              "111381732" :1A ; grimaldo (whatsapp con grimaldo)
+              "117729619" :1A ; grimaldo (whatsapp con grimaldo)
+              "118895855" :1A ; grimaldo (whatsapp con grimaldo)
+
+              ; aizaga
+              "84603534" :1C ; aizaga (whatsapp con aizaga)
+
+              ; lizca
+              "103132794" :3C ; lizca (whatsapp con lizca)
+              "98220251" :3C ; lizca (whatsapp con lizca)
+              "120843908" :3C ; lizca (whatsapp con lizca)
+              "129182651" :3C ; lizca (whatsapp con lizca)
+              "84603971" :3C ; lizca (whatsapp con lizca)
+              "186127688" :3C ; lizca (whatsapp con lizca)
+              "172037873" :3C ; lizca (whatsapp con lizca)
+            
             ;
               nil)]
     (if apt
@@ -180,21 +205,20 @@
        hack-l4
        hack-id
        hack-alvaro
-       assoc-parsed-amount
-       ))
+       assoc-parsed-amount))
 
 
 
-(comment 
-(Float/parseFloat "150")
-(Float/parseFloat "150.00")
-(Float/parseFloat "150")
-(Float/parseFloat "2400.04")  
+(comment
+  (Float/parseFloat "150")
+  (Float/parseFloat "150.00")
+  (Float/parseFloat "150")
+  (Float/parseFloat "2400.04")
   (assoc-parsed-amount {:amount "150.00"})
   (assoc-parsed-amount {:amount "2,400.40"})
   (assoc-parsed-amount {:amount ""})
   (assoc-parsed-amount {:amount nil})
-    
+
   ;
   )
 
